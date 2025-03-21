467a51540be891530c64b30ea91a5b36b3d0cc8c
Kishore_kumar.B <kishorekumar.b@bugbusterslabs.com>
2025-03-21 17:49:30 +0530

    TEST_2

--- jira/settings.py
+++ jira/settings.py
@@ -127,7 +127,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 100,
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
